2c6453af189d24c8c18712d51578850a8acf1c03
git <812788375@rqq.com>
2021-07-23 16:55:44 +0800

    添加TCP服务器的代码，TCP通信正常，发送包的长度正常，异常的

---
+++ TCPServer/message.h
@@ -0,0 +1,10 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+#include "mainwindow.h"
+typedef struct pack
+{
+    unsigned int total_data_size = 0;
+    unsigned int current_size = 0;
+    QString data;
+}PACK1 ;
+#endif // MESSAGE_H
